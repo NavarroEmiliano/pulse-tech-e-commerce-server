b8be539513dec7c3e813d20f180fd6dd8bb7f9ef
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-05-09 23:27:24 -0300

    samesite

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -16,7 +16,7 @@ const loginUserController = async (req, res) => {
     const tokenOption = {
       httpOnly: true,
       secure: true,
-      sameSite: 'None'
+      sameSite: 'Lax'
     }
 
     return res
